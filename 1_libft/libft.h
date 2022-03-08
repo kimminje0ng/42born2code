b9d2318c8ed176fd8d08f77b2892553712dcef1c
kimminje0ng <kimmin9642@dgu.ac.kr>
2022-03-08 17:25:58 +0900

    ADD libft/ft_is*.c

--- 1_libft/libft.h
+++ 1_libft/libft.h
@@ -1,1 +1,11 @@
-int ft_isalpha(char c);
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_isprint(int c);
+
+#endif LIBFT_H
