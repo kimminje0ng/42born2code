52f7c424f9202cbb170e136f6736fb67ed522fcb
kimminje0ng <kimmin9642@dgu.ac.kr>
2022-08-13 12:14:44 +0900

    2022-08-13 ADD get_next_line

---
+++ 3_get_next_line/get_next_line.h
@@ -0,0 +1,16 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+
+size_t	ft_strlen(const char *theString);
+
+#endif
